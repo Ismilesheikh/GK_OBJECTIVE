6d575b5e90c70a4564d9d895db1eba01e9d35404
Ismilesheikh <ismiledeveloper@gmail.com>
2024-09-16 15:10:57 +0530

    good

--- Physics/chapter 1/ch_1.js
+++ Physics/chapter 1/ch_1.js
@@ -1,2 +1,26 @@
 
-phy1a1.addEventListener('click', function onClick(event){event.target.style.backgroundColor='red';});
+//coded by Ismile sheikh//
+var seen = "Answer Seen"
+a1_1.addEventListener('click', function onClick(event) {
+  event.target.style.backgroundColor = 'red';
+  event.target.style.color = 'white';
+});
+b1_1.addEventListener('click', function onClick(event) {
+  event.target.style.backgroundColor = 'green';
+  event.target.style.color = 'white';
+});
+c1_1.addEventListener('click', function onClick(event) {
+  event.target.style.backgroundColor = 'red';
+  event.target.style.color = 'white';
+});
+d1_1.addEventListener('click', function onClick(event) {
+  event.target.style.backgroundColor = 'red';
+  event.target.style.color = 'white';
+});
+e1_1.addEventListener('click', function onClick(event) {
+  event.target.style.backgroundColor = 'green';
+  event.target.style.color = 'white';
+b1_1.style.backgroundColor = 'green';
+b1_1.style.color = 'white';
+e1_1.innerHTML = seen;
+});
